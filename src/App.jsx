8c94c1b00c847cb863d4d624438da4fb79b8a0ff
Ma'ruf <48182085+marufalafif@users.noreply.github.com>
2024-01-09 12:00:49 +0700

    Membuat fitur login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div className="App">
       <div className="navbar">
-        <h1>navbar tes</h1>
+        <div className="img-navbar">
+          <h1>navbar tes</h1>
+        </div>
+        <div className="btn-navbar">
+          <button>Login</button>
+          <button>Register</button>
+        </div>
       </div>
+      <Login />
       <Register />
     </div>
   );
