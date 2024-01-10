4ba7f4654f7191240218a61a6d7f328581840f0c
Ma'ruf <48182085+marufalafif@users.noreply.github.com>
2024-01-10 13:58:47 +0700

    Membuat Tampilan Reset Password

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
+import Login from "./pages/Login";
 import Register from "./pages/Register";
-// import ResetPassword from "./pages/ResetPassword";
-// import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+import CreatePassword from "./pages/CreatePassword";
 
 import "./App.css";
 
@@ -17,8 +18,10 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {/* <ResetPassword /> */}
+      <Login />
       <Register />
+      <CreatePassword />
+      <ResetPassword />
     </ThemeProvider>
   );
 };
